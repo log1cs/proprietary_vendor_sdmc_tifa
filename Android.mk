f7f577d6a73bef6c7a09b0a4e176b342307701aa
Tuan Anh <tuan73176@gmail.com>
2026-08-29 09:01:06 +0000

    oppen-user 14 UKG3.250803.001 20251124 release-keys
    Change-Id: Id17fdfd0be4121909dccff69c58e9b0f2340f20d

--- Android.mk
+++ Android.mk
@@ -4,8 +4,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),oppen)
+ifeq ($(TARGET_DEVICE),tifa)
 
-$(call add-radio-file-sha1-checked,radio/bootloader.img,2395ee1b53dbefd53afc6c80403158da51846d79)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,35960fa9e0f2ababeb267d3ca1e5a6ac9b0e5916)
 
 endif
