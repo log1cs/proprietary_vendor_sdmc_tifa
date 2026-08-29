754a037391a93f9cc9cc63b3da7cf18fb74beb7a
Tuan Anh <tuan73176@gmail.com>
2026-08-29 09:01:06 +0000

    oppen-user 14 UKG3.250803.001 20251124 release-keys
    Change-Id: Id17fdfd0be4121909dccff69c58e9b0f2340f20d

---
+++ tifa-vendor.mk
@@ -0,0 +1,61 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sdmc/tifa
+
+PRODUCT_COPY_FILES += \
+    vendor/sdmc/tifa/proprietary/product/etc/sysconfig/netflix.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/netflix.xml \
+    vendor/sdmc/tifa/proprietary/recovery/root/vendor/etc/mesondisplay.cfg:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/etc/mesondisplay.cfg \
+    vendor/sdmc/tifa/proprietary/vendor/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc \
+    vendor/sdmc/tifa/proprietary/vendor/etc/init/android.hardware.security.keymint-service.amlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.security.keymint-service.amlogic.rc \
+    vendor/sdmc/tifa/proprietary/vendor/etc/init/ref_provision.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ref_provision.rc \
+    vendor/sdmc/tifa/proprietary/vendor/etc/mesondisplay.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mesondisplay.cfg \
+    vendor/sdmc/tifa/proprietary/vendor/etc/permissions/android.hardware.hardware_keystore.amlogic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.amlogic.xml \
+    vendor/sdmc/tifa/proprietary/vendor/etc/permissions/droidlogic.software.netflix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/droidlogic.software.netflix.xml \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550001_2ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550001_2ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550001_3ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550001_3ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550002_2ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550002_2ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550002_3ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550002_3ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550003_2ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550003_2ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_23550003_3ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_23550003_3ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_56630000_2ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_56630000_2ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_56630000_3ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_56630000_3ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_56630001_2ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_56630001_2ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/etc/wifi/uwe5621/wifi_56630001_3ant.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/uwe5621/wifi_56630001_3ant.ini \
+    vendor/sdmc/tifa/proprietary/vendor/lib/firmware/aucpu_fw.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/aucpu_fw.bin \
+    vendor/sdmc/tifa/proprietary/vendor/lib/firmware/video/video_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/video/video_ucode.bin \
+    vendor/sdmc/tifa/proprietary/vendor/lib/firmware/wcnmodem.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/wcnmodem.bin \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/2088528d-102a-4716-b940-23fd9be04adf.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2088528d-102a-4716-b940-23fd9be04adf.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/5dc33b6a-216d-4641-a834-b3f3aa1accb8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/5dc33b6a-216d-4641-a834-b3f3aa1accb8.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/6c67ce8f-95c1-4826-b62e-1a857a1ced3d.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/6c67ce8f-95c1-4826-b62e-1a857a1ced3d.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/9a04f079-9840-4286-ab92-e65be0885f95.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/9a04f079-9840-4286-ab92-e65be0885f95.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/d83c3c4a-9e8d-4e4e-ad30-9d40e137f689.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/d83c3c4a-9e8d-4e4e-ad30-9d40e137f689.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta \
+    vendor/sdmc/tifa/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta
+
+PRODUCT_PACKAGES += \
+    lib_android_keymaster_akeymint_utils \
+    libacppcose_rkp \
+    libakeymint \
+    libakeymint_remote_prov_support \
+    libamavutils \
+    libbt-vendor \
+    liboemcrypto \
+    libsecmem \
+    libwifi-hal-uni \
+    vendor.ref.hardware.RefProvision@1.0 \
+    vendor.ref.hardware.RefProvision@1.1 \
+    libHwAudio_dcvdec \
+    libHwAudio_dtshd \
+    android.hardware.oemlock@1.0-service.droidlogic.xml \
+    android.hardware.security.keymint-service-no-rkp.amlogic.xml \
+    vendor.ref.hardware.RefProvision@1.1.xml \
+    android.hardware.oemlock@1.0-service.droidlogic \
+    android.hardware.security.keymint-service.amlogic \
+    ref_provision
